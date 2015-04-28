efdceb3439d5fbfac2730a38558f1894dfe949c9
Pawel Korus <pawelkorus@ymail.com>
2015-04-29 00:32:06 +0200

    Interator and operation interfaces

---
+++ index.js
@@ -0,0 +1,8 @@
+var core = require('./lib/core.js')
+var ta = require('./lib/ta.js')
+
+module.exports = exports = {
+	functionOperator: core.functionOperator,
+	arrayIterator: core.arrayIterator,
+	ta: ta
+}
---
+++ tests/core.js
@@ -0,0 +1,101 @@
+var expect = require('chai').expect
+var ta = require('../');
+
+var prepareRandomArray = function(n) {
+	n = n || 3;
+	
+	var testArray = [];
+	
+	while(n > 0) {
+		testArray.push(Math.random());
+		n--;
+	}
+
+	return testArray;
+}
+
+describe('arrayIterator tests', function() {
+
+it('iterating over elements', function(done) {
+	var testArray = prepareRandomArray(3);
+	var expectedHasNext = [true, true, true];
+	var it = ta.arrayIterator(testArray);
+
+	for(var i = 0; i < testArray.length; i++) {
+		expect(it.hasNext()).to.equal(expectedHasNext[i]);
+		expect(it.next()).to.equal(testArray[i]);
+	}
+
+	done();
+});
+
+it('iterating more times than there is elements in input array', function(done) {
+	var testArray = prepareRandomArray(3);
+	var expectedResults = [];
+	Array.prototype.push.apply(expectedResults, testArray);
+	Array.prototype.push.apply(expectedResults, [undefined, undefined]);
+	var expectedHasNext = [true, true, true, false, false];
+	
+	// we will iterate over testArray, but exepctedResults.length times
+	var it = ta.arrayIterator(testArray)
+
+	expectedResults.forEach(function(v, index) {
+		expect(it.hasNext()).to.equal(expectedHasNext[index])
+		expect(it.next()).to.equal(v);
+	})
+
+	done();
+});
+
+it('hasNext for empty array should give true', function(done) {
+	var testArray = [];
+
+	var it = ta.arrayIterator(testArray);
+
+	expect(it.hasNext()).to.be.false;
+
+	done();
+});
+
+});
+
+describe('function operator tests', function() {
+	
+it('operator over array of elements', function(done) {
+	var testArray = prepareRandomArray(5);
+	var testIt = ta.arrayIterator(testArray);
+	
+	var it = ta.arrayIterator(testArray);
+	var op = ta.functionOperator(it, function(v) {
+		expect(v).to.equal(testIt.next());
+		return v;
+	})
+
+	testArray.forEach(function(v) {
+		expect(op.next()).to.equal(v);
+	})
+	
+	done();
+})
+
+it('piped operators over array of elements', function(done) {
+	var testArray = prepareRandomArray(5);
+	var testIt = ta.arrayIterator(testArray);
+	
+	var it = ta.arrayIterator(testArray);
+	var op = ta.functionOperator(it, function(v) {
+		return v;
+	})
+	.pipe(function(v) {
+		expect(v).to.equal(testIt.next());
+		return v;
+	});
+	
+	testArray.forEach(function(v) {
+		expect(op.next()).to.equal(v);
+	})
+	
+	done();
+})
+	
+})
