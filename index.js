5fbee8004398ec26634436e1dd776fd76bc5d673
Pawel Korus <pawelkorus@ymail.com>
2015-05-12 14:39:39 +0200

    Function for calculating variance

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var core = require('./lib/core.js')
-var ta = require('./lib/ta.js')
+var core = require('./lib/core')
+var ta = require('./lib/ta')
 
 module.exports = exports = {
 	functionOperator: core.functionOperator,
 	arrayIterator: core.arrayIterator,
 	valueIterator: core.valueIterator,
-	ta: ta
+	ta: ta,
+	statistic: require('./lib/statistic')
 }
