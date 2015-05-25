9f81dd175b571042c47db8fb0c08907989df10b8
Pawel Korus <pawelkorus@ymail.com>
2015-05-25 15:29:15 +0200

    Util module with isInt method

---
+++ tests/util.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect;
+var util = require('../lib/util.js');
+
+describe('util', function() {
+
+var data = [
+//	inputs			outputs	function
+[	12,				true,	'isInt'	],
+[	12.3,			false,	'isInt'	],
+[	'aaaa',			false,	'isInt'	],
+[	undefined,		false,	'isInt'	],
+[	NaN,			false,	'isInt'	],
+[	0,				true,	'isInt'	],
+[	-23,			true,	'isInt'	],
+[	-0.24,			false,	'isInt'	],
+[	{},				false,	'isInt'	],
+// to pass array we need to put here array of parameters
+[	[ [] ],			false,	'isInt'	],
+[	function(){},	false,	'isInt'	]	
+]
+
+data.forEach(function(data) {
+
+var inputs = Array.isArray(data[0])? data[0] : [ data[0] ];
+var expectedOutput = data[1];
+var fun = util[data[2]];
+
+it('testing ' + data[2] + ' for ' + inputs, function(done) {
+	var output = fun.apply(null, inputs);
+
+	expect(output).to.equal(expectedOutput);
+	
+	done();
+})
+
+})
+
+})
